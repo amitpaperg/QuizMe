9d6b106618f5435a788495366d01b72118846bcb
Amit Velingkar <amit.velingkar@paperg.com>
2017-04-29 06:32:08 -0700

    Initialization
    updated react, added material-ui and added firebase config

--- src/app/config.js
+++ src/app/config.js
@@ -3,11 +3,12 @@ module.exports = {
   // Change this to your firebase configuration! (Add Firebase to your web app)
     FIREBASE_CONFIG: {
 
-        apiKey: 'AIzaSyAAzWIkcMqyHueh1yQt84HdpMFfzL1OZYc',
-        authDomain: 'react-redux-firebase-d6283.firebaseapp.com',
-        databaseURL: 'https://react-redux-firebase-d6283.firebaseio.com',
-        storageBucket: 'react-redux-firebase-d6283.appspot.com',
-        messagingSenderId: '425723634684',
+        apiKey: 'AIzaSyCw_icLxj2Q0BDd50JgscT2bDHQG7FqPn8',
+        authDomain: 'quizme-5ce41.firebaseapp.com',
+        databaseURL: 'https://quizme-5ce41.firebaseio.com',
+        projectId: 'quizme-5ce41',
+        storageBucket: 'quizme-5ce41.appspot.com',
+        messagingSenderId: '300597882057',
 
     },
 };
